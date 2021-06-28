3b62ca24b21f01866e8428cff23084873f09433a
Gweneth Malmquist <gdmalmquist@gmail.com>
2021-06-28 13:42:25 -0400

    fixing cyan color

--- pi-in-a-jar/code/lights.py
+++ pi-in-a-jar/code/lights.py
@@ -96,7 +96,7 @@ def parse_color(c):
     'trans-white': WHITE,
     'blue': (0, 0, 255),
     'green': (0, 255, 0),
-    'cyan': (0, 255, 0),
+    'cyan': (0, 255, 255),
     'magenta': (255, 0, 255),
     'red': (255, 0, 0),
     'yellow': (255, 255, 0),
