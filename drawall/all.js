23bfcfd29d18f0edefdcbc9ddc49f8c8e48dc3cc
Gwen Malmquist <gdmalmquist@gmail.com>
2023-04-07 14:55:20 -0400

    bugfixes~

--- drawall/all.js
+++ drawall/all.js
@@ -1131,6 +1131,7 @@ class SDF {
         let traveled = Distance(0, distance.space);
         while (distance.get(distance.space) > eps) {
             point = point.splus(distance, direction);
+            traveled = traveled.plus(distance);
             const newDistance = this.sdist(point);
             if (newDistance.gt(distance)) {
                 // we're getting farther away, treat this as a miss
@@ -5967,10 +5968,12 @@ class UiState {
         if (this.dragging) {
             this.renderSnap();
         }
-        for (const s of this.selection) {
-            for (const lever of s.entity.get(Lever)) {
-                if (lever.visible) {
-                    this.renderLever(lever);
+        if (this.selection.size === 1) {
+            for (const s of this.selection) {
+                for (const lever of s.entity.get(Lever)) {
+                    if (lever.visible) {
+                        this.renderLever(lever);
+                    }
                 }
             }
         }
@@ -6081,7 +6084,7 @@ class UiState {
     }
     selectAll() {
         this.setSelection(...App.ecs.getComponents(Handle)
-            .filter(h => h.selectable && h.isForTool(App.tools.current.name)));
+            .filter(h => h.selectable && h.visible && !h.control && h.isForTool(App.tools.current.name)));
     }
     deleteSelected() {
         const selected = this.selection;
@@ -10446,6 +10449,9 @@ class PointerTool extends Tool {
     }
     renderKnobs() {
         const selectionSize = App.ui.selection.size;
+        const hovered = App.ecs.getComponents(Hovered);
+        if (hovered.length > 2)
+            return;
         const handleDrawRadius = Distance(100, 'screen');
         const shouldRender = (h) => {
             if (h.knob === null)
@@ -10453,7 +10459,10 @@ class PointerTool extends Tool {
             if (h.dragging)
                 return true;
             const parent = h.knob.parent.only(Handle);
-            return selectionSize <= 1 && parent.isActive;
+            return selectionSize <= 1 && parent.isActive && hovered.every(hovered => {
+                const handle = hovered.entity.only(Handle);
+                return handle === parent || handle === h;
+            });
         };
         const handles = App.ecs.getComponents(Handle).filter(shouldRender);
         for (const h of handles) {
@@ -10479,18 +10488,15 @@ class PointerTool extends Tool {
                 const strict = this.useStrictSelectFor(e);
                 this.strictSelect.set(strict);
                 const selectables = App.ecs.getComponents(Handle)
-                    .filter(h => h.selectable && h.isForTool(this.name));
+                    .filter(h => h.selectable && h.visible && !h.control && h.isForTool(this.name));
                 for (const s of selectables) {
                     s.hovered = strict ? s.containedBy(rect) : s.intersects(rect);
                 }
             },
             onEnd: (e, _) => {
                 App.pane.style.cursor = 'default';
-                const rect = rectFor(e);
-                const strict = this.useStrictSelectFor(e);
-                const selected = App.ecs.getComponents(Handle)
-                    .filter(h => h.selectable && h.isForTool(this.name))
-                    .filter(h => strict ? h.containedBy(rect) : h.intersects(rect));
+                const selected = App.ecs.getComponents(Hovered)
+                    .map(h => h.entity.only(Handle));
                 App.ui.clearHovered();
                 App.ui.addSelection(...selected);
                 this.selectionRect.set(null);
