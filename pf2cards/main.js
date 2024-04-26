090492973c883cee6ecb17271f93efe743e6af97
Gwen Malmquist <gdmalmquist@gmail.com>
2024-04-26 00:27:12 +0000

    pf2cards

---
+++ pf2cards/main.js
@@ -0,0 +1,26 @@
+function generateCards() {
+  const postUrl = window.location.hostname === 'gwenscode.com'
+    ? 'http://146.190.55.168:8120/'
+    : '/'
+  ;
+  const names = Array.from(new Set(Array.from(document.getElementsByClassName('spellnames'))
+    .map(e => e.value)[0]
+    .trim()
+    .split("\n")
+    .map(e => e.trim().toLocaleLowerCase())
+    .filter(e => e.length > 0)));
+  names.sort();
+  document.write('Loading, this may take several seconds ...');
+  fetch(postUrl, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(names),
+  }).then(x => x.text()).then(x => {
+    document.open()
+    document.write(x)
+  });
+}
+
