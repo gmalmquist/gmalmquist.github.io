805f9f81afd00e654425c7d33548a5a813b1b8cd
Gwen Malmquist <gdmalmquist@gmail.com>
2024-04-26 01:49:02 +0000

    update pf2cards domain

--- pf2cards/main.js
+++ pf2cards/main.js
@@ -1,6 +1,6 @@
 function generateCards() {
   const postUrl = window.location.hostname === 'gwenscode.com'
-    ? 'https://146.190.55.168/pf2cards/'
+    ? 'https://gwen.run/pf2cards/'
     : '/'
   ;
   const names = Array.from(new Set(Array.from(document.getElementsByClassName('spellnames'))
