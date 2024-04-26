0a06bcb35ec93f32bfeed48bf6ee9defc6db92ec
Gwen Malmquist <gdmalmquist@gmail.com>
2024-04-26 02:05:56 +0000

    saving spell list in cookies

--- pf2cards/main.js
+++ pf2cards/main.js
@@ -1,3 +1,16 @@
+setTimeout(() => {
+  // read cookie
+  const cookies = decodeURIComponent(document.cookie);
+  const textbox = Array.from(new Set(Array.from(document.getElementsByClassName('spellnames'))))[0];
+  for (const cookie of cookies.split(";")) {
+    const parts = cookie.split("=");
+    if (parts.length > 0 && parts[0].trim() == "names") {
+      textbox.value = JSON.parse(parts[1].trim()).join("\n");
+      break;
+    }
+  }
+}, 500);
+
 function generateCards() {
   const postUrl = window.location.hostname === 'gwenscode.com'
     ? 'https://gwen.run/pf2cards/'
@@ -10,6 +23,7 @@ function generateCards() {
     .map(e => e.trim().toLocaleLowerCase())
     .filter(e => e.length > 0)));
   names.sort();
+  document.cookie = "names=" + JSON.stringify(names) + ";";
   document.write('Loading, this may take several seconds ...');
   fetch(postUrl, {
     method: 'POST',
