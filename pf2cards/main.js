e67a6485d2803a92906ad1d226fda94c279de816
Gwen Malmquist <gdmalmquist@gmail.com>
2024-04-26 18:07:44 +0000

    columning

--- pf2cards/main.js
+++ pf2cards/main.js
@@ -22,6 +22,26 @@ setTimeout(() => {
   }
 }, 500);
 
+function shrinkText() {
+  const cards = Array.from(document.getElementsByClassName('card'));
+  let shrunk = false;
+  for (const card of cards) {
+    const cardRect = card.getBoundingClientRect();
+    const cardBody = Array.from(card.getElementsByClassName("card-body"))[0];
+    const bodyRect = cardBody.getBoundingClientRect();
+    if (
+      typeof cardRect !== 'undefined'
+      && typeof bodyRect !== 'undefined'
+      && bodyRect.bottom > cardRect.bottom
+    ) {
+      const fs = getComputedStyle(cardBody).getPropertyValue("font-size");
+      card.style.fontSize = `calc(${fs} - 0.5pt)`;
+      shrunk = true;
+    }
+  }
+  return shrunk;
+}
+
 function generateCards() {
   const postUrl = window.location.hostname === 'gwenscode.com'
     ? 'https://gwen.run/pf2cards/'
@@ -43,8 +63,10 @@ function generateCards() {
     value: cardSizeOption.value,
     width: cardSizeOption.getAttribute("data-width"),
     height: cardSizeOption.getAttribute("data-height"),
-    'body-text': cardSizeOption.getAttribute('data-body-text'),
-    'head-text': cardSizeOption.getAttribute('data-head-text'),
+    corner: cardSizeOption.getAttribute("data-corner"),
+    columns: parseInt(cardSizeOption.getAttribute("data-cols")),
+    "body-text": cardSizeOption.getAttribute("data-body-text"),
+    "head-text": cardSizeOption.getAttribute("data-head-text"),
   };
 
   document.cookie = "card-size=" + JSON.stringify(cardSizeValue) + ";";
@@ -61,11 +83,67 @@ function generateCards() {
     document.open();
     document.write(x);
     const r = document.querySelector(':root');
-    r.style.setProperty('--card-width', cardSizeValue.width);
-    r.style.setProperty('--card-height', cardSizeValue.height);
-    r.style.setProperty('--body-text', cardSizeValue['body-text']);
-    r.style.setProperty('--head-text', cardSizeValue['head-text']);
-    console.log('card size', cardSizeValue);
+    const props = {
+      "--card-width": cardSizeValue.width,
+      "--card-height": cardSizeValue.height,
+      "--corner-radius": cardSizeValue.corner,
+      "--card-columns": cardSizeValue.columns,
+      "--body-text": cardSizeValue["body-text"],
+      "--head-text": cardSizeValue["head-text"],
+    };
+    if (cardSizeValue.columns === 2) {
+      props['--twos-h0'] = 'var(--page-margin)';
+      props['--twos-v0'] = 'var(--page-margin)';
+      props['--twos-h1'] = `calc((
+        var(--page-width)
+        - var(--page-margin) * 2
+        - var(--card-width) * ${cardSizeValue.columns}
+      ) / ${cardSizeValue.columns - 1} - 1pt)`
+      .replaceAll('\\s', '');
+      props['--twos-v1'] = `calc((
+        var(--page-height)
+        - var(--page-margin) * 2
+        - var(--card-height) * 2
+      ) - 1pt)`
+      .replaceAll('\\s', '');
+      props['--twos-break'] = 'always';
+    }
+    if (cardSizeValue.columns === 3) {
+      props['--threes-h0'] = 'var(--page-margin)';
+      props['--threes-v0'] = 'var(--page-margin)';
+      props['--threes-h1'] = `calc((
+        var(--page-width)
+        - var(--page-margin) * 2
+        - var(--card-width) * ${cardSizeValue.columns}
+      ) / ${cardSizeValue.columns - 1} - 1pt)`
+      .replaceAll('\\s', '');
+      props['--threes-v1'] = `calc((
+        var(--page-height)
+        - var(--page-margin) * 2
+        - var(--card-height) * 2
+      ) - 1pt)`
+      .replaceAll('\\s', '');
+      props['--threes-break'] = 'always';
+    }
+    for (const key of Object.keys(props)) {
+      r.style.setProperty(key, props[key]);
+    }
+    r.style.setProperty("--card-width", cardSizeValue.width);
+    r.style.setProperty("--card-height", cardSizeValue.height);
+    r.style.setProperty("--body-text", cardSizeValue["body-text"]);
+    r.style.setProperty("--head-text", cardSizeValue["head-text"]);
+    r.style.setProperty("--corner-radius", cardSizeValue.corner);
+    const shrink = () => setTimeout(() => {
+      if (shrinkText()) {
+        shrink();
+      }
+    }, 5);
+    setTimeout(() => {
+      for (const card of document.getElementsByClassName('card')) {
+        card.classList.add(`card${cardSizeValue.columns}`);
+      }
+    }, 5);
+    shrink();
   });
 }
 
